e896bb54bbd91a8af4e25946c3c1bd8b9427cbd1
dscimeca <dscimeca@cj.com>
2018-05-31 11:46:25 -0700

    basic request parsing

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,11 @@ import Network (listenOn, PortID(PortNumber))
 import Network.Socket (accept, withSocketsDo, close, Socket)
 import Network.Socket.ByteString (recv, sendAll)
 import Control.Concurrent (forkIO)
+import Data.List.Split
+import Data.Map.Lazy ()
 import qualified Data.ByteString.Lazy.Char8 as LazyByte (toChunks)
 import qualified Data.ByteString as  ByteString (concat)
+import qualified Data.ByteString.Char8 as Char8 (unpack)
 import qualified Data.Text.Lazy as LazyText (Text, length, pack, concat)
 import Data.Text.Lazy.Encoding (encodeUtf8)
 
@@ -24,12 +27,37 @@ loop sock = do
   where
     respond conn = do
       msg <- recv conn 1024
-      print msg
+      request <- getRequest msg
+      print request
       let resp = okResp "hello world!"
       sendAll conn (ByteString.concat . LazyByte.toChunks $ toByteString resp)
       close conn
+    getRequest msg = do
+       let charMsg = Char8.unpack msg
+       let requestSplit = splitOn "\r\n" charMsg
+
+       let mainString = requestSplit!!0
+       let mainSplit = splitOn " " mainString
+       let method = mainSplit!!0
+       let path = mainSplit!!1
+       let httpVersion = mainSplit!!2
+
+       let requestWithoutMain = tail requestSplit
+       let requestTrim = init requestWithoutMain
+       let requestTrim2 = init requestTrim
+       let headers = map (splitOn ": ") requestTrim2
+
+       let request = HttpRequest{ method = method, path = path, httpVersion = httpVersion, headers = headers}
+
+       return request
 
 
+data Request = HttpRequest{
+    method :: [Char],
+    path :: [Char],
+    httpVersion :: [Char],
+    headers :: [[[Char]]]
+} deriving( Show, Eq )
 
 data Response = TextResponse {
   version :: LazyText.Text,
@@ -40,6 +68,10 @@ data Response = TextResponse {
 
 okResp text = TextResponse {
   version = "1.1", status = 200, reason = "Ok", text = text
+}
+
+notFoundResp text reason = TextResponse {
+  version = "1.1", status = 404, reason = reason, text = text
 }
 
 toByteString (TextResponse {
